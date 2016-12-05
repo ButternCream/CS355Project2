4b3f70ceb0955f540d765942d957bcf0e40cd012
Scott Mitchell <mitchsco@sonoma.edu>
2016-12-05 14:54:28 -0800

    Looks pretty

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project 2 Game Servers', description: 'This website will contain a list of game servers to browse' });
+  res.render('index', { title: 'Project 2 Game Servers', description: 'This website will contain a list of game servers and information related to game servers.' });
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about');
 });
 
 module.exports = router;
