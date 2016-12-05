f2232c7fcf129357537a283ffbdd57e7796ea29f
Scott <smitchell@athleticboard.com>
2016-12-04 16:21:12 -0800

    Added Server Add/Delete functionality (Add isn't perfect)

--- model/server_dal.js
+++ model/server_dal.js
@@ -6,7 +6,14 @@ var connection = mysql.createConnection(db.config);
 
 /* Get all the games */
 exports.getAll = function(callback){
-    var query = 'SELECT * FROM game_server';
+    var query = 'SELECT * FROM ServerInfo';
+    connection.query(query, function(err, result){
+        callback(err, result);
+    });
+};
+
+exports.getGames = function(callback){
+    var query = 'SELECT game_name, dev_name FROM game;';
     connection.query(query, function(err, result){
         callback(err, result);
     });
@@ -18,4 +25,22 @@ exports.getByName = function(server_ip, callback){
     connection.query(query, queryData, function(err ,result){
         callback(err , result);
     });
+};
+
+exports.insert = function(params, callback){
+  var query = 'INSERT INTO game_server (server_IP, server_name, server_status, dev_name_server, game_name_server) VALUES (?,?,?,?,?)';
+  var queryData = [params.server_ip, params.server_name, params.server_status, params.dev_name_server, params.game_name_server];
+  connection.query(query,queryData,function(err, result){
+     callback(err, result);
+  });
+};
+
+exports.delete = function(server_ip, callback) {
+    var query = 'DELETE FROM game_server WHERE server_IP = ?';
+    var queryData = [server_ip];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
 };
--- routes/server_routes.js
+++ routes/server_routes.js
@@ -28,5 +28,62 @@ router.get('/', function(req, res){
     }
 });
 
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    server_dal.getGames(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('server/serverAdd', {'games': result});
+        }
+    });
+});
+
+router.get('/insert', function(req, res){
+   if (req.query.server_ip == ""){
+       res.send('server_ip is null');
+   }
+   else if (req.query.server_name == ""){
+       res.send('server_name is null');
+   }
+   else if (req.query.server_status == null){
+       res.send('server_status is null');
+   }
+   else if (req.query.dev_name_server == null){
+       res.send('dev_name_server is null');
+   }
+   else if (req.query.game_name_server == null){
+       res.send('game_name_server is null');
+   }
+   else{
+       server_dal.insert(req.query, function(err, result){
+          if (err){
+              res.send(err);
+          }
+          else{
+              res.redirect(302, '/server/all');
+          }
+       });
+   }
+
+});
+
+router.get('/delete', function(req, res){
+    if(req.query.server_ip == null) {
+        res.send('server_ip is null');
+    }
+    else {
+        server_dal.delete(req.query.server_ip, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/server/all');
+            }
+        });
+    }
+});
 
 module.exports = router;
